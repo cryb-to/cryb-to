d508e9ebc2f25dbdc312b4b480441eb30732ff04
Dag-Erling Smørgrav <des@des.no>
2014-07-13 21:34:54 +0000

    Missing newline character

--- t/t_strlcat.c
+++ t/t_strlcat.c
@@ -139,7 +139,7 @@ t_strlcat(char **desc CRYB_UNUSED, void *arg)
 	buf[T_BUFSIZE] = T_CANARY;
 	sz = strlcat(buf, t->in, T_BUFSIZE);
 	if (buf[T_BUFSIZE] != T_CANARY) {
-		t_verbose("buffer overflow");
+		t_verbose("buffer overflow\n");
 		return (0);
 	}
 	ret = t_compare_sz(t->sz, sz);
