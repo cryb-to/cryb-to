cc8109c760e84b7e00ec45ed4a96557f9fdf6e16
Dag-Erling Smørgrav <des@des.no>
2014-08-01 13:55:01 +0000

    WIP: unit tests for managed strings.

---
+++ t/t_wstring.c
@@ -0,0 +1,82 @@
+/*-
+ * Copyright (c) 2014 Dag-Erling Smørgrav
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Author: Dag-Erling Smørgrav <des@des.no>
+ *
+ * $Cryb: trunk/t/t_ctype.c 8 2014-07-10 13:17:47Z des $
+ */
+
+#include "cryb/impl.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <wchar.h>
+
+#include <cryb/wstring.h>
+
+#include "t.h"
+
+#define char_t			wchar_t
+
+#define cryb_string		cryb_wstring
+#define string			wstring
+
+#define string_new		wstring_new
+#define string_expand		wstring_expand
+#define string_shrink		wstring_shrink
+#define string_dup		wstring_dup
+#define string_delete		wstring_delete
+#define string_trunc		wstring_trunc
+#define string_append_c		wstring_append_wc
+#define string_append_cs	wstring_append_wcs
+#define string_append_string	wstring_append_wstring
+#define string_printf		wstring_printf
+#define string_vprintf		wstring_vprintf
+
+#include "t__string.c"
+
+
+/***************************************************************************
+ * Boilerplate
+ */
+
+int
+t_prepare(int argc, char *argv[])
+{
+
+	(void)argc;
+	(void)argv;
+	t_add_test(t_foo, NULL, "foo");
+	return (0);
+}
+
+void
+t_cleanup(void)
+{
+}
