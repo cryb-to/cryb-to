b9cf4fb8358f56abeb87dca02adc6338dd5362f0
Dag-Erling Smørgrav <d.e.smorgrav@usit.uio.no>
2016-09-15 11:18:01 +0200

    Implement the full range of BSD endianness conversion functions.

--- t/t_endian.c
+++ t/t_endian.c
@@ -35,6 +35,11 @@
 #include <stdio.h>
 #include <string.h>
 
+/* test our own code, not the compiler's */
+#undef HAVE___BUILTIN_BSWAP16
+#undef HAVE___BUILTIN_BSWAP32
+#undef HAVE___BUILTIN_BSWAP64
+
 #include <cryb/endian.h>
 
 #include <cryb/test.h>
@@ -45,9 +50,26 @@ static uint64_t be64 = 0xdeadbeefcafef001ULL;
 static uint16_t le16 = 0x01f0U;
 static uint32_t le32 = 0xefbeaddeUL;
 static uint64_t le64 = 0x01f0fecaefbeaddeULL;
+static uint16_t h16;
+static uint32_t h32;
+static uint64_t h64;
 static const void *s16 = "\xf0\x01";
 static const void *s32 = "\xde\xad\xbe\xef";
 static const void *s64 = "\xde\xad\xbe\xef\xca\xfe\xf0\x01";
+
+#define T_BSWAP(w)							\
+	static int							\
+	t_bswap##w(char **desc CRYB_UNUSED, void *arg CRYB_UNUSED)	\
+	{								\
+		uint##w##_t swap;					\
+									\
+		swap = cryb_bswap##w(be##w);				\
+		return (t_compare_x##w(le##w, swap));			\
+	}
+
+T_BSWAP(16);
+T_BSWAP(32);
+T_BSWAP(64);
 
 #define T_DEC(e, w)							\
 	static int							\
@@ -83,15 +105,55 @@ T_ENC(le, 16)
 T_ENC(le, 32)
 T_ENC(le, 64)
 
+#define T_HTO(e, w)							\
+	static int							\
+	t_hto##e##w(char **desc CRYB_UNUSED, void *arg CRYB_UNUSED)	\
+	{								\
+		uint##w##_t e;						\
+									\
+		e = cryb_hto##e##w(h##w);				\
+		return (t_compare_x##w(e##w, e));			\
+	}
+
+T_HTO(be, 16);
+T_HTO(be, 32);
+T_HTO(be, 64);
+T_HTO(le, 16);
+T_HTO(le, 32);
+T_HTO(le, 64);
+
+#define T_TOH(e, w)							\
+	static int							\
+	t_##e##w##toh(char **desc CRYB_UNUSED, void *arg CRYB_UNUSED)	\
+	{								\
+		uint##w##_t h;						\
+									\
+		h = cryb_##e##w##toh(e##w);				\
+		return (t_compare_x##w(h##w, h));			\
+	}
+
+T_TOH(be, 16);
+T_TOH(be, 32);
+T_TOH(be, 64);
+T_TOH(le, 16);
+T_TOH(le, 32);
+T_TOH(le, 64);
+	
 
 /***************************************************************************
  * Boilerplate
  */
 
+#define T_BSWAP_ADD(w)							\
+	t_add_test(t_bswap##w, 0, "bswap" #w);
 #define T_DEC_ADD(e, w)							\
 	t_add_test(t_##e##w##dec, 0, #e #w "dec");
 #define T_ENC_ADD(e, w)							\
 	t_add_test(t_##e##w##enc, 0, #e #w "enc");
+#define T_HTO_ADD(e, w)							\
+	t_add_test(t_hto##e##w, 0, "hto" #e #w);
+#define T_TOH_ADD(e, w)							\
+	t_add_test(t_##e##w##toh, 0, #e #w "toh");
 
 static int
 t_prepare(int argc, char *argv[])
@@ -99,6 +161,20 @@ t_prepare(int argc, char *argv[])
 
 	(void)argc;
 	(void)argv;
+
+#if WORD_BIGENDIAN
+	h16 = be16;
+	h32 = be32;
+	h64 = be64;
+#else
+	h16 = le16;
+	h32 = le32;
+	h64 = le64;
+#endif
+
+	T_BSWAP_ADD(16);
+	T_BSWAP_ADD(32);
+	T_BSWAP_ADD(64);
 	T_DEC_ADD(be, 16);
 	T_DEC_ADD(be, 32);
 	T_DEC_ADD(be, 64);
@@ -111,6 +187,18 @@ t_prepare(int argc, char *argv[])
 	T_ENC_ADD(le, 16);
 	T_ENC_ADD(le, 32);
 	T_ENC_ADD(le, 64);
+	T_HTO_ADD(be, 16);
+	T_HTO_ADD(be, 32);
+	T_HTO_ADD(be, 64);
+	T_HTO_ADD(le, 16);
+	T_HTO_ADD(le, 32);
+	T_HTO_ADD(le, 64);
+	T_TOH_ADD(be, 16);
+	T_TOH_ADD(be, 32);
+	T_TOH_ADD(be, 64);
+	T_TOH_ADD(le, 16);
+	T_TOH_ADD(le, 32);
+	T_TOH_ADD(le, 64);
 	return (0);
 }
 
