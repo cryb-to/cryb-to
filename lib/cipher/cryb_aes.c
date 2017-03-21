93af1846936267bb91f8af3f9bb8f7fbf9948bb1
Dag-Erling Smørgrav <des@des.no>
2017-03-21 13:24:02 +0100

    Remove unused struct member.

--- lib/cipher/cryb_aes.c
+++ lib/cipher/cryb_aes.c
@@ -581,8 +581,7 @@ aes_init(aes_ctx *ctx, cipher_mode mode, const uint8_t *key, size_t keylen)
 	assert(mode == CIPHER_MODE_ENCRYPT || mode == CIPHER_MODE_DECRYPT);
 	assert(keylen == 16 || keylen == 24 || keylen == 32);
 	memset(ctx, 0, sizeof *ctx);
-	ctx->mode = mode;
-	if (ctx->mode == CIPHER_MODE_DECRYPT)
+	if (mode == CIPHER_MODE_DECRYPT)
 		aes_setkey_dec(ctx, key, keylen);
 	else
 		aes_setkey_enc(ctx, key, keylen);
