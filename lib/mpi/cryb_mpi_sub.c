7e05fe419a82c00620621673080a85dfe452d4b2
Dag-Erling Smørgrav <des@des.no>
2017-03-29 18:09:36 +0200

    Remove unused headers.

--- lib/mpi/cryb_mpi_sub.c
+++ lib/mpi/cryb_mpi_sub.c
@@ -29,8 +29,8 @@
 
 #include "cryb/impl.h"
 
+#include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
 
 #include <cryb/mpi.h>
 
--- lib/mpi/cryb_mpi_swap.c
+++ lib/mpi/cryb_mpi_swap.c
@@ -29,9 +29,8 @@
 
 #include "cryb/impl.h"
 
+#include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include <cryb/endian.h>
