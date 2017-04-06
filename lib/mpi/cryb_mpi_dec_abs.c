0bc4da2e6ab778ea49697d1f6cdbbcbba1b97b37
Dag-Erling Smørgrav <des@des.no>
2017-04-06 17:50:39 +0200

    Implement fast increment / decrement functions.

---
+++ lib/mpi/cryb_mpi_dec_abs.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2017 Dag-Erling Smørgrav
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "cryb/impl.h"
+
+#include <errno.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <strings.h>
+
+#include <cryb/bitwise.h>
+#include <cryb/mpi.h>
+
+#include "cryb_mpi_impl.h"
+
+/*
+ * Decrement X in place.
+ */
+int
+mpi_dec_abs(cryb_mpi *X)
+{
+	unsigned int i;
+
+	/* trivial case: zero */
+	if (X->msb == 0) {
+		errno = EINVAL;
+		return (-1);
+	}
+
+	/* decrement least-significant word and propagate */
+	for (i = 0; X->words[i]-- == 0; ++i)
+		/* nothing */;
+	i = X->msb / 32;
+	if (X->words[i] == 0)
+		--i;
+	X->msb = i * 32 + flsl(X->words[i]);
+
+	return (0);
+}
